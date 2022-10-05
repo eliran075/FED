3de5b7f3b633153ba82c093f5f2200b7f8e3f474
eliran075 <84390133+eliran075@users.noreply.github.com>
2022-10-05 19:32:52 +0200

    added navbar + hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var openButton = document.querySelector("header > nav > button");
+
+openButton.addEventListener("click", openMenu);
+
+function openMenu() {
+    var deNav = document.querySelector("header nav ul:first-of-type");
+    deNav.classList.toggle("toonMenu");
+    console.log("omg");
+}
